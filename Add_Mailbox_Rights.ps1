37366922ed12baf2854e8ee17ce4eabbcb3379e6
chadrothermel <49419484+chadrothermel@users.noreply.github.com>
2020-02-17 17:30:53 -0500

    Add files via upload

---
+++ Add_Mailbox_Rights.ps1
@@ -0,0 +1,20 @@
+﻿Connect-ExchangeOnlineShell
+
+$mailboxes = import-csv '\\mpbnas01\Info_Tech\Powershell\Mail Migration\Mailbox_Rights\FullAccess_MailboxRights.csv'
+
+Foreach($mailbox in $mailboxes){
+
+    Add-MailboxPermission -Identity $mailbox.Mailbox -User $mailbox.User  -AccessRights $mailbox.AccessRights -InheritanceType All
+
+}
+
+#Add-MailboxPermission -Identity newsflash@midpennbank.com -User dawn.spahr@midpennbank.com -AccessRights FullAccess, ReadPermission -InheritanceType All
+
+
+New-DistributionGroup -Name "Loan Ops Calendar - Edit" -Alias loanopscaledit -Type security
+add-MailboxFolderPermission loanopscalendar@midpennbank.com -User loanopscaledit -AccessRights editor
+
+
+$Loanops = import-csv \\mpbnas01\Info_Tech\loanopscal.csv
+
+$Loanops | %{Add-DistributionGroupMember -Identity loanopscaledit -Member $_.member}
---
+++ Remove_Mailbox_Rights.ps1
@@ -0,0 +1,9 @@
+﻿#Connect-ExchangeOnlineShell
+
+$mailboxes = import-csv '\\mpbnas01\Info_Tech\mailbox_permissions.csv'
+
+Foreach($mailbox in $mailboxes){
+
+    Remove-MailboxPermission -Identity $mailbox.Mailbox -User $mailbox.User  -AccessRights $mailbox.AccessRights -InheritanceType All
+
+}
